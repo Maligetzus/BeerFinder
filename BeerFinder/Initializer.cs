4fc2928360058b963c973349aeb3277a3ffc59a1
Maligetzus <32730953+Maligetzus@users.noreply.github.com>
2018-02-19 12:44:27 +0100

    Final version

---
+++ BeerFinder/Initializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BeerFinder.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeerFinder
+{
+    public class Initializer
+    {
+        public static async void Initialize(IServiceProvider services)
+        {
+            using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var _manager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                var _roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                
+                IdentityRole role=new IdentityRole("admin");
+                await _roleManager.CreateAsync(role);
+
+                var user = new ApplicationUser()
+                {
+                    UserName = "hrvoje.maligec@fer.hr",
+                    Email = "hrvoje.maligec@fer.hr",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+                IdentityResult check = await _manager.CreateAsync(user, "$ifrA123");
+                if (check.Succeeded)
+                {
+                    await _manager.AddToRoleAsync(user, "admin");
+                }
+            }
+        }
+    }
+}
